90f8ac29d31952051226b1d4d592471019ec5a4f
Maxime Thirouin <m@moox.io>
2013-12-16 11:01:51 +0100

    Add talks/slides in this repo

---
+++ src/slides/reveal.js/config.rb
@@ -0,0 +1,11 @@
+#http_path = '/'
+css_dir = 'css'
+sass_dir = 'css'
+#fonts_dir = 'fonts'
+#images_dir = 'images'
+
+line_comments = false
+relative_assets = true
+
+require "compass-normalize";
+require "compass-recipes";
