b69ccc4d054ba132ebc4e32c73d3ede7169565ac
Maxime Thirouin <m@moox.io>
2014-02-15 08:21:57 +0100

    Brighter design
    Close #44

--- src/assets/_scripts/vanilla.js
+++ src/assets/_scripts/vanilla.js
@@ -6,21 +6,22 @@ document.documentElement.classList.toggle('no-js');
 
 // call me just before bottom, I'll be dom ready :p
 [].forEach.call(document.querySelectorAll('[href="#not-ready"]'), function(el){
-	el.setAttribute('data-hint', 'Not ready yet !');
+  el.setAttribute('data-hint', 'Not ready yet !');
 });
 
 [].forEach.call(document.querySelectorAll('.js-togglable'), function(el){
-	var toggler = el.querySelector('.js-togglable__toggler');
-	var items = [].slice.call(el.querySelectorAll('.js-togglable__item'));
-	
-	var toggle = function(event) {
-		items.forEach(function(item) {
-			item.classList.toggle('js-togglable__item--hide');
-		});
-	};
-	
-	toggler.addEventListener('click', toggle);
-	toggler.addEventListener('keyup', toggle);
+  var toggler = el.querySelector('.js-togglable__toggler');
+  var items = [].slice.call(el.querySelectorAll('.js-togglable__item'));
+
+  var toggle = function(event) {
+    el.classList.toggle('js-togglable--toggled');
+    items.forEach(function(item) {
+      item.classList.toggle('js-togglable__item--hide');
+    });
+  };
+
+  toggler.addEventListener('click', toggle);
+  toggler.addEventListener('keyup', toggle);
 });
 
 // everything run fine, let's tell this to our stylesheets
