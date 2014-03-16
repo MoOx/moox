daa8af8425e2ccf101a7a4e66f002dce27e0beb8
Maxime Thirouin <m@moox.io>
2014-03-16 07:59:10 +0100

    New design (v9)
    Accessible with old IEs.

--- src/assets/_scripts/vanilla.js
+++ src/assets/_scripts/vanilla.js
@@ -2,32 +2,28 @@
 document.documentElement.classList.toggle("js");
 document.documentElement.classList.toggle("no-js");
 
-// my website
-
 // call me just before bottom, I"ll be dom ready :p
-;[].forEach.call(document.querySelectorAll("[href=\"#not-ready\"]"), function(el) {
-  el.setAttribute("data-tip", "Not ready yet !");
-})
+if (document.querySelector && document.documentElement.classList) {
+  ;[].forEach.call(document.getElementsByClassName("js-Togglable"), function(el) {
+    var toggler = el.querySelector(".js-Togglable-toggler")
+    var items = [].slice.call(el.getElementsByClassName("js-Togglable-item"))
 
-;[].forEach.call(document.getElementsByClassName("js-Togglable"), function(el) {
-  var toggler = el.querySelector(".js-Togglable-toggler");
-  var items = [].slice.call(el.getElementsByClassName("js-Togglable-item"));
+    var toggle = function(event) {
+      el.classList.toggle("js-Togglable--toggled")
+      items.forEach(function(item) {
+        item.classList.toggle("js-Togglable-item--hide")
+      })
+    }
 
-  var toggle = function(event) {
-    el.classList.toggle("js-Togglable--toggled");
-    items.forEach(function(item) {
-      item.classList.toggle("js-Togglable-item--hide");
-    });
-  };
+    toggler.addEventListener("click", toggle)
+    toggler.addEventListener("keyup", toggle)
+  })
 
-  toggler.addEventListener("click", toggle);
-  toggler.addEventListener("keyup", toggle);
-});
+  ;[].forEach.call(document.getElementsByClassName("js-MailTo"), function(el) {
+    el.setAttribute("href", "mailto:" + el.getAttribute("data-mailto-user") + "@" + (el.getAttribute("data-mailto-domain") || window.location.host))
+  })
 
-;[].forEach.call(document.getElementsByClassName("js-MailTo"), function(el) {
-  el.setAttribute("href", "mailto:" + el.getAttribute("data-mailto-user") + "@" + (el.getAttribute("data-mailto-domain") || window.location.host))
-})
-
-// everything run fine, let's tell this to our stylesheets
-document.documentElement.classList.toggle('js');
-document.documentElement.classList.toggle('no-js');
+  // everything run fine, let's tell this to our stylesheets
+  document.documentElement.classList.toggle('js');
+  document.documentElement.classList.toggle('no-js');
+}
