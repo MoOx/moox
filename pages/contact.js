a3c9e03b49eb14e13765bb95d6b37303f225d251
Maxime Thirouin <git@moox.io>
2021-03-13 18:59:09 +0100

    Add missing contact page js proxy

---
+++ pages/contact.js
@@ -0,0 +1,4 @@
+// export { getStaticPaths } from "../src/pages/PageContact.bs.js";
+// export { getStaticProps } from "../src/pages/PageContact.bs.js";
+import page from "../src/pages/PageContact.bs.js";
+export default page;
