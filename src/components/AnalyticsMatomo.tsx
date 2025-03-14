a5e534aeb6d71ad13ff6723171c913ba1c92e696
Max Thirouin <git@moox.io>
2025-03-14 07:49:36 +0100

    analytics

---
+++ src/components/AnalyticsMatomo.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { init, push } from "@socialgouv/matomo-next";
+import { usePathname, useSearchParams } from "next/navigation";
+import * as React from "react";
+
+const MATOMO_OPTIONS = {
+  url: "https://a.moox.fr",
+  jsTrackerFile: "m.js",
+  phpTrackerFile: "m.php",
+  siteId: "3",
+};
+
+const Matomo = () => {
+  const [initialised, setInitialised] = React.useState(false);
+
+  React.useEffect(() => {
+    if (!initialised) {
+      init(MATOMO_OPTIONS);
+    }
+    return () => {
+      setInitialised(true);
+    };
+  }, [initialised, setInitialised]);
+
+  const searchParams = useSearchParams()?.toString();
+  const pathname = usePathname();
+  React.useEffect(() => {
+    if (!pathname) return;
+
+    const url = pathname + (searchParams ? "?" + searchParams : "");
+    push(["setCustomUrl", url]);
+    push(["trackPageView"]);
+  }, [pathname, searchParams]);
+
+  return null;
+};
+
+export default function AnalyticsMatomo() {
+  return (
+    <React.Suspense>
+      <Matomo />
+    </React.Suspense>
+  );
+}
